b5baf07a6bea790266c73404b551691f4fe1a50a
Marco DeLao <325707742+Marco-DeLao@users.noreply.github.com>
2026-09-09 12:16:36 +0300

    Delete target directory

--- target/release/deps/pkgclean-9da40001cbc57dc1.d
+++
@@ -1,5 +0,0 @@
-/home/Hamza/work/pkgclean/target/release/deps/pkgclean-9da40001cbc57dc1.d: src/main.rs
-
-/home/Hamza/work/pkgclean/target/release/deps/pkgclean-9da40001cbc57dc1: src/main.rs
-
-src/main.rs:
--- target/release/pkgclean.d
+++
@@ -1,1 +0,0 @@
-/home/Hamza/work/pkgclean/target/release/pkgclean: /home/Hamza/work/pkgclean/src/main.rs
